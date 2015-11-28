c7a30b022b6582079c76884f49fa7bbf4e81e5d2
AKB428 <gandara016+1@gmail.com>
2015-11-28 19:03:11 +0900

    fix live

--- live_client.rb
+++ live_client.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'net/https'
 require 'csv'
 require './lib/niconico'
+require 'pp'
 
 #https://gist.github.com/mpppk/118a3dcf93324429fb1e
 #ref http://blog.livedoor.jp/mgpn/archives/51886270.html
@@ -19,12 +20,27 @@ end
 NICO_MAIL  = ARGV[0]
 NICO_PASS  = ARGV[1]
 MOVIE_ID   = ARGV[2]
-DST_DIR    = "comments"
 
+DST_DIR    = 'comments'
 Dir.mkdir(DST_DIR) unless File.exists?(DST_DIR)
 
-nico = NicoNico.new
-nico.login(NICO_MAIL, NICO_PASS)
-info = nico.getplayerstatus(MOVIE_ID)
+def live_init
+  nico = NicoNico.new
+  nico.login(NICO_MAIL, NICO_PASS)
+  info = nico.getplayerstatus(MOVIE_ID)
 
-p info
+  #pp info
+
+  @hostname = info['getplayerstatus']['ms']['addr']
+  @port =  info['getplayerstatus']['ms']['port']
+  @thread_id = info['getplayerstatus']['ms']['thread']
+
+  puts @hostname
+  puts @port
+  puts @thread_id
+
+  time_st = Time.now.strftime("%Y%m%d-%H%M%S")
+  @filename = "#{DST_DIR}/#{@thread_id}_#{time_st}_comments.tsv"
+end
+
+live_init
