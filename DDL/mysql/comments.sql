27bd7ab5628970ed7329295bf8767e96bbc9d762
AKB428 <gandara016+1@gmail.com>
2015-12-06 15:26:21 +0900

    add MySQL DDL

---
+++ DDL/mysql/comments.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `comments` (
+  `thread` int,
+  `no` int,
+  `vpos` int,
+  `date` int,
+  `mail`varchar(100),
+  `user_id` varchar(100),
+  `premium` varchar(5),
+  `anonymity` varchar(5),
+  `leaf` varchar(5),
+  `fork` varchar(5),
+  `deleted` varchar(5),
+  `content` varchar(255),
+  KEY `comments_thread` (`thread`),
+  KEY `comments_date` (`date`),
+  KEY `comments_user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
